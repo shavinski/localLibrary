f4f60bbc07de018c67ccedcd2db6d00f10c9d18a
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-05-01 13:11:46 -0700

    Add schemas and models for book, bookinstance and genre
    Create populatedb.js to populate database

---
+++ models/genre.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+  name: { type: String, required: true, maxlength: 100, minlength: 3 },
+});
+
+GenreSchema.virtual("url").get(function () {
+  return `catalog/genre/${name}`;
+});
+
+module.exports = mongoose.model("Genre", GenreSchema);
