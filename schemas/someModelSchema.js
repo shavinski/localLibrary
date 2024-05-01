4dc13107daadfe0bd7f1c1463211f2db431a4f3b
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-04-30 19:49:32 -0700

    Add schemas folder, add DB connection to mongoDB

---
+++ schemas/someModelSchema.js
@@ -0,0 +1,15 @@
+// File: ./models/somemodel.js
+
+// Require Mongoose
+const mongoose = require("mongoose");
+
+// Define a schema
+const Schema = mongoose.Schema;
+
+const SomeModelSchema = new Schema({
+  a_string: String,
+  a_date: Date,
+});
+
+// Export function to create "SomeModel" model class
+module.exports = mongoose.model("someModel", SomeModelSchema);
