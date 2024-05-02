c8fdef6444140e9ad1749e0f379575a41b0b7b0f
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-05-01 19:23:33 -0700

    Add controllers folder
    Add express async handler dependency

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// ------------------ ROUTES --------------------
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
