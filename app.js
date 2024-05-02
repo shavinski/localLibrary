c6099882e7f90783e890caec81aa0e9bde9d092d
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-05-01 19:34:49 -0700

    Add catalog route with routes to all endpoints with controller functions
    Fix error with bookInstance file name
    Add controller functions for all new controller files

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
 const app = express();
 require("dotenv").config();
@@ -33,6 +33,8 @@ app.use(express.static(path.join(__dirname, "public")));
 // ------------------ ROUTES --------------------
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
